83b9b65c31f7ee250c9c041dd773b911db75196b
NesaOktavia <nesaoktavia10@gmail.com>
2022-10-31 11:44:10 +0700

    tampilan

---
+++ application/views/templates/sidebar.php
@@ -0,0 +1,119 @@
+<!-- Sidebar -->
+<ul class="navbar-nav sidebar sidebar-dark accordion font-weight-bold" style="background: #254F43;"
+    id="accordionSidebar">
+    
+    <!-- Sidebar - LOGO-->
+    <div class="d-flex justify-content-center">
+        <!-- logo info -->
+        <div class=" logo_pic">
+            <img src="<?php echo base_url('assets/img/profile/Logo.png') ?>" alt="..." class="img-circle" width="200"
+                height="200" style= "position:relative; margin-top:20px;"></a>
+        </div>
+    </div>
+    <div class="profile">
+        <div class="font-weight-bold text-center" style="color: white; margin-bottom:20px;"><span style="font-size: 20px;">
+                Toko Adriel
+            </span></div>
+    </div>
+
+    <!-- Nav Item - Beranda -->
+    <li class="nav-item">
+        <a class="nav-link" href="<?= base_url('user/index')?>">
+            <i class="fas fa-fw fa-home"></i>
+            <span>Beranda</span></a>
+    </li>
+
+    <!-- Divider Garis -->
+    <hr class="sidebar-divider">
+
+
+    <!-- Menu Obat -->
+    <li class="nav-item <?php echo $this->uri->segment(2) == 'lihat_obat'||'form_obat'||'tabel_kedaluwarsa'||'tabel_stok' ? 'active': '' ?>">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseObat" aria-expanded="true"
+            aria-controls="collapseObat">
+            <i class="fa fa-fw fa-medkit"></i>
+            <span>Obat</span>
+        </a>
+        <div id="collapseObat" class="collapse" aria-labelledby="headingObat" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <a class="collapse-item <?php echo $this->uri->segment(2) == 'form_obat' ? 'active': '' ?>" href="<?= base_url('user/form_obat')?>">Tambah Obat</a>
+                <a class="collapse-item <?php echo $this->uri->segment(2) == 'lihat_obat' ? 'active': '' ?>" href="<?= base_url('user/lihat_obat')?>">Lihat Obat</a>
+                <a class="collapse-item <?php echo $this->uri->segment(2) == 'tabel_kedaluwarsa' ? 'active': '' ?>" href="<?= base_url('user/tabel_kedaluwarsa')?>">Obat Kedaluwarsa</a>
+                <a class="collapse-item <?php echo $this->uri->segment(2) == 'tabel_stok' ? 'active': '' ?>" href="<?= base_url('user/tabel_stok')?>">Obat Akan Habis</a>
+            </div>
+        </div>
+    </li>
+
+    <!-- Menu Kategori -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseKategori"
+            aria-expanded="true" aria-controls="collapseKategori">
+            <i class="fa fa-fw fa-plus-square"></i>
+            <span>Kategori</span>
+        </a>
+        <div id="collapseKategori" class="collapse" aria-labelledby="headingKategori" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <a class="collapse-item" href="<?= base_url('user/form_kategori')?>">Tambah Kategori</a>
+                <a class="collapse-item" href="<?= base_url('user/lihat_kategori')?>">Lihat Kategori</a>
+            </div>
+        </div>
+    </li>
+
+    <!-- Menu Pemasok -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePemasok"
+            aria-expanded="true" aria-controls="collapsePemasok">
+            <i class="fa fa-fw fa-users"></i>
+            <span>Pemasok</span>
+        </a>
+        <div id="collapsePemasok" class="collapse" aria-labelledby="headingPemasok" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <a class="collapse-item" href="<?= base_url('user/form_pemasok')?>">Tambah Pemasok</a>
+                <a class="collapse-item" href="<?= base_url('user/lihat_pemasok')?>">Lihat Pemasok</a>
+            </div>
+        </div>
+    </li>
+
+    <!-- Menu Transaksi -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTransaksi"
+            aria-expanded="true" aria-controls="collapseTransaksi">
+            <i class="fa fa-fw fa-shopping-cart"></i>
+            <span>Transaksi</span>
+        </a>
+        <div id="collapseTransaksi" class="collapse" aria-labelledby="headingTransaksi" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <a class="collapse-item" href="<?= base_url('user/lihat_penjualan')?>">Penjualan</a>
+                <a class="collapse-item" href="<?= base_url('user/lihat_pembelian')?>">Pembelian</a>
+            </div>
+        </div>
+    </li>
+
+
+    <!-- Menu LAPORAN -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseLaporan"
+            aria-expanded="true" aria-controls="collapseLaporan">
+            <i class="fas fa-fw fa-chart-area"></i>
+            <span>Laporan</span>
+        </a>
+        <div id="collapseLaporan" class="collapse" aria-labelledby="headingLaporan" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <a class="collapse-item" href="<?= base_url('laporan_controller/laporan_penjualan')?>">Laporan
+                    Penjualan</a>
+                <a class="collapse-item" href="<?= base_url('laporan_controller/laporan_pembelian')?>"> Laporan
+                    Pembelian</a>
+            </div>
+        </div>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block">
+
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+
+</ul>
+<!-- End of Sidebar -->
---
+++ application/views/user/index.php
@@ -0,0 +1,188 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Header Keterangan -->
+    <div class="row tile_count justify-content-center" style="text-align:center">
+        <div class=" col-md-2 col-sm-4 col-xs-6 tile_stats_count p-1 m-1" style="font-size:20px;">
+            <span class="count_top"><i class="fa fa-medkit"></i> Total Obat</span>
+            <div class="count" style="text-align: ;"><?php echo $sumObat ?></div>
+        </div>
+        <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count p-1 m-1" style="font-size:20px;">
+            <span class="count_top"><i class="fa fa-plus-square"></i> Total Kategori</span>
+            <div class="count" style="text-align: ;"><?php echo $sumKat ?></div>
+        </div>
+        <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count p-1 m-1" style="font-size:20px;">
+            <span class="count_top"><i class="fa fa-users"></i> Total Pemasok</span>
+            <div class="count" style="text-align: ;"><?php echo $sumPemasok ?></div>
+        </div>
+        <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count p-1 m-1" style="font-size:20px;">
+            <span class="count_top"><i class="fa fa-edit"></i> Total Penjualan</span>
+            <div class="count" style="text-align: ;"><?php echo number_format($sumJual) ?></div>
+        </div>
+        <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count p-1 m-1" style="font-size:20px;">
+            <span class="count_top"><i class="fa fa-edit"></i> Total Pembelian</span>
+            <div class="count" style="text-align: ;"><?php echo number_format($sumBeli) ?></div>
+        </div>
+    </div>
+    <!-- /Header Keterangan -->
+
+    <!-- Body -->
+    <div class="row pt-5">
+        <div class="col-xl-3 col-md-6 mb-4">
+            <a href="<?= base_url('user/form_obat')?>" class="text-decoration-none">
+                <div class="card border-left-primary shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-lg-start font-weight-bold text-primary text-uppercase mb-1">
+                                    Obat
+                                </div>
+                                <div class="text-sm-start text-gray-80">Menambahkan <br>Obat</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-medkit fa-2x text-gray-300 size"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-xl-3 col-md-6 mb-4">
+            <a href="<?= base_url('user/form_kategori')?>" class="text-decoration-none">
+                <div class="card border-left-success shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-lg-start font-weight-bold text-success text-uppercase mb-1">
+                                    Kategori</div>
+                                <div class="text-sm-start text-gray-80">Menambahkan <br> kategori</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fa fa-plus-square fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-xl-3 col-md-6 mb-4">
+            <a href="<?= base_url('user/form_pemasok')?>" class="text-decoration-none">
+                <div class="card border-left-info shadow h-s100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-lg-start font-weight-bold text-info text-uppercase mb-1">
+                                    Pemasok
+                                </div>
+                                <div class="text-sm-start text-gray-80">Menambahkan <br> pemasok</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fa fa-users fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-xl-3 col-md-6 mb-4">
+            <a href="<?= base_url('user/form_penjualan')?>" class="text-decoration-none">
+                <div class="card border-left-warning shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-lg-start font-weight-bold text-warning text-uppercase mb-1">
+                                    Penjualan</div>
+                                <div class="text-sm-start text-gray-80">Menambahkan penjualan</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-shopping-cart fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+
+    <!-- BARIS KE 2 -->
+    <div class="row">
+        <div class="col-xl-3 col-md-6 mb-4">
+            <a href="<?= base_url('user/tabel_kedaluwarsa')?>" class="text-decoration-none">
+                <div class="card border-left-warning shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-lg-start font-weight-bold text-warning text-uppercase mb-1">
+                                    Kedaluwarsa</div>
+                                <div class="text-sm-start text-gray-80">Menampilkan obat kedaluwarsa</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fa fa-exclamation-triangle fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-xl-3 col-md-6 mb-4">
+            <a href="<?= base_url('user/tabel_stok')?>" class="text-decoration-none">
+                <div class="card border-left-info shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-lg-start font-weight-bold text-info text-uppercase mb-1">Habis
+                                </div>
+                                <div class="text-sm-start text-gray-80">Menampilkan obat akan habis</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-xl-3 col-md-6 mb-4">
+            <a href="<?= base_url('laporan_controller/laporan_penjualan')?>" class="text-decoration-none">
+                <div class="card border-left-success shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-lg-start font-weight-bold text-success text-uppercase mb-1">
+                                    Laporan</div>
+                                <div class="text-sm-start text-gray-80">Menampilkan <br> laporan</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-chart-area fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-xl-3 col-md-6 mb-4">
+            <a href="<?= base_url('user/form_pembelian')?>" class="text-decoration-none">
+                <div class="card border-left-primary shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-lg-start font-weight-bold text-primary text-uppercase mb-1">
+                                    Pembelian</div>
+                                <div class="text-sm-start text-gray-80">Menambahkan pembelian</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+
+</div>
+<!-- /.container-fluid -->
+
+<script src="<?= base_url('assets/');?>vendor/moment/min/moment.min.js">
+</script>
