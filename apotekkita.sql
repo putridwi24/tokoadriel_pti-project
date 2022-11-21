a1c159f243de9d77c9956a92021c849a50fd5945
NesaOktavia <nesaoktavia10@gmail.com>
2022-11-22 00:35:20 +0700

    database update

--- apotekkita.sql
+++ apotekkita.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 17 Bulan Mei 2022 pada 15.35
--- Versi server: 10.4.24-MariaDB
--- Versi PHP: 8.0.3
+-- Generation Time: Nov 21, 2022 at 06:32 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `month`
+-- Table structure for table `month`
 --
 
 CREATE TABLE `month` (
@@ -33,7 +33,7 @@ CREATE TABLE `month` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `month`
+-- Dumping data for table `month`
 --
 
 INSERT INTO `month` (`month_num`, `month_name`) VALUES
@@ -53,7 +53,36 @@ INSERT INTO `month` (`month_num`, `month_name`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_kategori`
+-- Table structure for table `tb_barang`
+--
+
+CREATE TABLE `tb_barang` (
+  `id` int(11) NOT NULL,
+  `nama_barang` varchar(128) NOT NULL,
+  `stok` int(11) NOT NULL,
+  `kedaluwarsa` date NOT NULL,
+  `h_jual` int(11) NOT NULL,
+  `h_beli` int(11) NOT NULL,
+  `nama_supplier` varchar(128) NOT NULL,
+  `nama_kat` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_barang`
+--
+
+INSERT INTO `tb_barang` (`id`, `nama_barang`, `stok`, `kedaluwarsa`, `h_jual`, `h_beli`, `nama_supplier`, `nama_kat`) VALUES
+(63, 'Aqua', 10, '2022-11-30', 5000, 4000, 'Bina San Prima', 'Makanan'),
+(75, 'Coca Cola', 3, '2022-11-29', 9000, 8999, 'S', 'Minuman'),
+(76, 'Sprite', 3, '2022-11-30', 10000, 9000, 'S', 'Minuman'),
+(77, 'Fanta', 9, '2023-12-20', 6000, 5500, 'Sari Roti', 'Minuman'),
+(78, 'Pepsi', 28, '0000-00-00', 7000, 8000, 'Bina San Prima', 'Minuman'),
+(80, 'Pepsi', 6, '0000-00-00', 7000, 8000, 'Bina San Prima', 'Minuman');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_kategori`
 --
 
 CREATE TABLE `tb_kategori` (
@@ -63,20 +92,17 @@ CREATE TABLE `tb_kategori` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `tb_kategori`
+-- Dumping data for table `tb_kategori`
 --
 
 INSERT INTO `tb_kategori` (`id_kat`, `nama_kat`, `desk_kat`) VALUES
-(45, 'Antibiotik', 'Obat pencegah infeksi'),
-(46, 'Demam', 'Obat untuk demam/nyeri'),
-(47, 'Darah Tinggi', 'Obat menurunkan tekanan darah'),
-(48, 'Obat Batuk', 'Obat batuk berdahak dan kering'),
-(49, 'Maag', 'Obat meredakan sakit maag');
+(45, 'Minuman', 'Air mineral botol'),
+(52, 'Makanan', 'aaa');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_obat`
+-- Table structure for table `tb_obat`
 --
 
 CREATE TABLE `tb_obat` (
@@ -92,7 +118,7 @@ CREATE TABLE `tb_obat` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `tb_obat`
+-- Dumping data for table `tb_obat`
 --
 
 INSERT INTO `tb_obat` (`id`, `nama_obat`, `penyimpanan`, `stok`, `kedaluwarsa`, `h_jual`, `h_beli`, `nama_pemasok`, `nama_kat`) VALUES
@@ -112,7 +138,7 @@ INSERT INTO `tb_obat` (`id`, `nama_obat`, `penyimpanan`, `stok`, `kedaluwarsa`, 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_pemasok`
+-- Table structure for table `tb_pemasok`
 --
 
 CREATE TABLE `tb_pemasok` (
@@ -123,7 +149,7 @@ CREATE TABLE `tb_pemasok` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `tb_pemasok`
+-- Dumping data for table `tb_pemasok`
 --
 
 INSERT INTO `tb_pemasok` (`id_pemasok`, `nama_pemasok`, `alamat_pemasok`, `telepon_pemasok`) VALUES
@@ -136,39 +162,44 @@ INSERT INTO `tb_pemasok` (`id_pemasok`, `nama_pemasok`, `alamat_pemasok`, `telep
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_pembelian`
+-- Table structure for table `tb_pembelian`
 --
 
 CREATE TABLE `tb_pembelian` (
   `id_beli` int(11) NOT NULL,
   `ref` varchar(128) NOT NULL,
-  `nama_obat` varchar(128) NOT NULL,
+  `nama_barang` varchar(128) NOT NULL,
   `h_beli` int(11) NOT NULL,
+  `h_jual` int(11) NOT NULL,
   `banyak` int(11) NOT NULL,
   `subtotal` int(11) NOT NULL,
-  `nama_pemasok` varchar(128) NOT NULL,
+  `nama_supplier` varchar(128) NOT NULL,
   `tgl_beli` date NOT NULL,
-  `grandtotal` int(11) NOT NULL
+  `grandtotal` int(11) NOT NULL,
+  `kedaluwarsa` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `tb_pembelian`
+-- Dumping data for table `tb_pembelian`
 --
 
-INSERT INTO `tb_pembelian` (`id_beli`, `ref`, `nama_obat`, `h_beli`, `banyak`, `subtotal`, `nama_pemasok`, `tgl_beli`, `grandtotal`) VALUES
-(6, 'PjPAEcXjdd', 'OBH', 10000, 1, 10000, 'Belibis', '2022-05-16', 22500),
-(7, 'PjPAEcXjdd', 'Ambroxol', 2500, 5, 12500, 'Belibis', '2022-05-16', 22500);
+INSERT INTO `tb_pembelian` (`id_beli`, `ref`, `nama_barang`, `h_beli`, `h_jual`, `banyak`, `subtotal`, `nama_supplier`, `tgl_beli`, `grandtotal`, `kedaluwarsa`) VALUES
+(30, 'bjUdFPnxQ7', 'Pepsi', 8000, 9000, 9, 72000, 'Bina San Prima', '2022-11-08', 72000, '2022-11-30'),
+(31, 'BoQhlLLZGZ', 'Aqua', 4000, 5000, 1, 4000, 'Bina San Prima', '2022-11-13', 12000, '2022-11-30'),
+(32, 'BoQhlLLZGZ', 'Pepsi', 8000, 7000, 1, 8000, 'Bina San Prima', '2022-11-13', 12000, '0000-00-00'),
+(33, 'obvrZ8uEWG', 'Fanta', 5500, 6000, 1, 5500, 'Sari Roti', '2023-06-19', 5500, '2023-12-20');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_penjualan`
+-- Table structure for table `tb_penjualan`
 --
 
 CREATE TABLE `tb_penjualan` (
   `id_jual` int(11) NOT NULL,
+  `id_barang` int(11) NOT NULL,
   `ref` varchar(128) NOT NULL,
-  `nama_obat` varchar(128) NOT NULL,
+  `nama_barang` varchar(128) NOT NULL,
   `h_beli` int(11) NOT NULL,
   `banyak` int(11) NOT NULL,
   `subtotal` int(11) NOT NULL,
@@ -178,20 +209,45 @@ CREATE TABLE `tb_penjualan` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `tb_penjualan`
+-- Dumping data for table `tb_penjualan`
 --
 
-INSERT INTO `tb_penjualan` (`id_jual`, `ref`, `nama_obat`, `h_beli`, `banyak`, `subtotal`, `nama_pembeli`, `tgl_beli`, `grandtotal`) VALUES
-(15, 'VKCgXFVryj', 'OBH', 10000, 1, 10000, 'Candra', '2022-05-16', 16000),
-(16, 'VKCgXFVryj', 'Paracetamol', 3000, 2, 6000, 'Candra', '2022-05-16', 16000),
-(17, '6nWppmAcFk', 'Ambroxol', 2500, 4, 10000, 'Candra Lagi Beli', '2022-05-17', 29000),
-(18, '6nWppmAcFk', 'OBH', 10000, 1, 10000, 'Candra Lagi Beli', '2022-05-17', 29000),
-(19, '6nWppmAcFk', 'Paracetamol', 3000, 3, 9000, 'Candra Lagi Beli', '2022-05-17', 29000);
+INSERT INTO `tb_penjualan` (`id_jual`, `id_barang`, `ref`, `nama_barang`, `h_beli`, `banyak`, `subtotal`, `nama_pembeli`, `tgl_beli`, `grandtotal`) VALUES
+(86, 80, 'glIRtHUgFL', 'Pepsi', 8000, 4, 32000, 'samsul', '2022-11-20', 32000),
+(87, 75, 'n0UfansUKd', 'Coca Cola', 8999, 6, 53994, 'Denis', '2022-10-05', 162994),
+(88, 76, 'n0UfansUKd', 'Sprite', 9000, 5, 45000, 'Denis', '2022-10-05', 162994),
+(89, 80, 'n0UfansUKd', 'Pepsi', 8000, 8, 64000, 'Denis', '2022-10-05', 162994);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `user`
+-- Table structure for table `tb_supplier`
+--
+
+CREATE TABLE `tb_supplier` (
+  `id_supplier` int(11) NOT NULL,
+  `nama_supplier` varchar(128) NOT NULL,
+  `alamat_supplier` varchar(128) NOT NULL,
+  `telepon_supplier` int(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_supplier`
+--
+
+INSERT INTO `tb_supplier` (`id_supplier`, `nama_supplier`, `alamat_supplier`, `telepon_supplier`) VALUES
+(27, 'Mensa Binasukses', 'Kedamaian, Tanjung karang Timur, Bandar Lampung', 721253690),
+(28, 'Belibis', 'JALAN UDANG I NO 29, Garuntang, Kec. Bumi Waras, Kota Bandar Lampung, Lampung 35227', 721472153),
+(29, 'Bina San Prima', 'JL P. Tembesu No.16 A, Campang Raya, Kec. Tanjungkarang Timur, Kota Bandar Lampung, Lampung 35122', 721788664),
+(30, 'Sapta Sari Tama', 'Jl. Arif Rahman Hakim Blok B No.20, Way Halim Permai, Kec. Sukarame, Kota Bandar Lampung, Lampung 35133', 721706896),
+(31, 'Antarmitra Sembada', 'Jl. Cut Nyak Dien No.76, Palapa, Kec. Tj. Karang Pusat, Kota Bandar Lampung, Lampung 35119', 721263087),
+(33, 'Sari Roti', 'Jl. Nusa Indah', 2147483647),
+(34, 'a', 'a', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
@@ -206,17 +262,19 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `user`
+-- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id`, `name`, `email`, `image`, `password`, `role_id`, `is_active`, `date_created`) VALUES
 (3, 'Candra Wahyu F', 'candra@gmail.com', 'default.png', '$2y$10$3uGOaFUpOQO4UWpt9f24.OO2UUP1Ab/FZIVvnP5A9ltG/FBaERwqG', 1, 1, 1649111986),
-(4, 'admin', 'admin@gmail.com', 'default.png', '$2y$10$Roy4l/1.yx7GgGZcCL76lefSC2VtJ5T3tu2SaRRc7F56FPTrxg5FG', 1, 1, 1649148911);
+(4, 'admin', 'admin@gmail.com', 'default.png', '$2y$10$Roy4l/1.yx7GgGZcCL76lefSC2VtJ5T3tu2SaRRc7F56FPTrxg5FG', 1, 1, 1649148911),
+(7, 'Nesa Oktavia', 'nesaoktavia10@gmail.com', 'default.png', '$2y$10$TFcqzB0iGJE0uD9200sjiOdKpch2dZ70eSWCfP5U8RqBKZwnrY95C', 1, 1, 1668852314),
+(8, 'Nesa Oktavia', 'nesaoktavia20@gmail.com', 'default.png', '$2y$10$0wLlUrr0vKxJ2C/FQjAJ4eXCgvq36hSCnPV3iCPTwbwlv1BD9vpJi', 1, 1, 1668864395);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `user_role`
+-- Table structure for table `user_role`
 --
 
 CREATE TABLE `user_role` (
@@ -225,7 +283,7 @@ CREATE TABLE `user_role` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `user_role`
+-- Dumping data for table `user_role`
 --
 
 INSERT INTO `user_role` (`id`, `role`) VALUES
@@ -237,20 +295,30 @@ INSERT INTO `user_role` (`id`, `role`) VALUES
 --
 
 --
--- Indeks untuk tabel `month`
+-- Indexes for table `month`
 --
 ALTER TABLE `month`
   ADD PRIMARY KEY (`month_num`);
 
 --
--- Indeks untuk tabel `tb_kategori`
+-- Indexes for table `tb_barang`
+--
+ALTER TABLE `tb_barang`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_kat` (`nama_kat`),
+  ADD KEY `nama_kat` (`nama_kat`),
+  ADD KEY `nama_obat` (`nama_barang`),
+  ADD KEY `nama_supplier` (`nama_supplier`) USING BTREE;
+
+--
+-- Indexes for table `tb_kategori`
 --
 ALTER TABLE `tb_kategori`
   ADD PRIMARY KEY (`id_kat`),
   ADD UNIQUE KEY `nama_kat` (`nama_kat`);
 
 --
--- Indeks untuk tabel `tb_obat`
+-- Indexes for table `tb_obat`
 --
 ALTER TABLE `tb_obat`
   ADD PRIMARY KEY (`id`),
@@ -260,81 +328,100 @@ ALTER TABLE `tb_obat`
   ADD KEY `nama_obat` (`nama_obat`);
 
 --
--- Indeks untuk tabel `tb_pemasok`
+-- Indexes for table `tb_pemasok`
 --
 ALTER TABLE `tb_pemasok`
   ADD PRIMARY KEY (`id_pemasok`),
   ADD KEY `nama_pemasok` (`nama_pemasok`);
 
 --
--- Indeks untuk tabel `tb_pembelian`
+-- Indexes for table `tb_pembelian`
 --
 ALTER TABLE `tb_pembelian`
   ADD PRIMARY KEY (`id_beli`),
-  ADD KEY `nama_pemasok` (`nama_pemasok`),
-  ADD KEY `nama_obat` (`nama_obat`);
+  ADD KEY `nama_supplier` (`nama_supplier`) USING BTREE,
+  ADD KEY `nama_barang` (`nama_barang`) USING BTREE;
 
 --
--- Indeks untuk tabel `tb_penjualan`
+-- Indexes for table `tb_penjualan`
 --
 ALTER TABLE `tb_penjualan`
   ADD PRIMARY KEY (`id_jual`),
-  ADD KEY `nama_obat` (`nama_obat`);
+  ADD KEY `nama_obat` (`nama_barang`);
 
 --
--- Indeks untuk tabel `user`
+-- Indexes for table `tb_supplier`
+--
+ALTER TABLE `tb_supplier`
+  ADD PRIMARY KEY (`id_supplier`),
+  ADD KEY `nama_supplier` (`nama_supplier`) USING BTREE;
+
+--
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `user_role`
+-- Indexes for table `user_role`
 --
 ALTER TABLE `user_role`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `tb_kategori`
+-- AUTO_INCREMENT for table `tb_barang`
+--
+ALTER TABLE `tb_barang`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=81;
+
+--
+-- AUTO_INCREMENT for table `tb_kategori`
 --
 ALTER TABLE `tb_kategori`
-  MODIFY `id_kat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
+  MODIFY `id_kat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
 
 --
--- AUTO_INCREMENT untuk tabel `tb_obat`
+-- AUTO_INCREMENT for table `tb_obat`
 --
 ALTER TABLE `tb_obat`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=76;
 
 --
--- AUTO_INCREMENT untuk tabel `tb_pemasok`
+-- AUTO_INCREMENT for table `tb_pemasok`
 --
 ALTER TABLE `tb_pemasok`
   MODIFY `id_pemasok` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
--- AUTO_INCREMENT untuk tabel `tb_pembelian`
+-- AUTO_INCREMENT for table `tb_pembelian`
 --
 ALTER TABLE `tb_pembelian`
-  MODIFY `id_beli` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_beli` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
--- AUTO_INCREMENT untuk tabel `tb_penjualan`
+-- AUTO_INCREMENT for table `tb_penjualan`
 --
 ALTER TABLE `tb_penjualan`
-  MODIFY `id_jual` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id_jual` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=90;
 
 --
--- AUTO_INCREMENT untuk tabel `user`
+-- AUTO_INCREMENT for table `tb_supplier`
+--
+ALTER TABLE `tb_supplier`
+  MODIFY `id_supplier` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+
+--
+-- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
--- AUTO_INCREMENT untuk tabel `user_role`
+-- AUTO_INCREMENT for table `user_role`
 --
 ALTER TABLE `user_role`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
